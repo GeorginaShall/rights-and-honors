8599bbd8a36ef37e10ab5491e4f387758974d2ba
GeorginaShall <georgina.shall@creoshift.com>
2025-07-22 17:51:38 +0300

    larger

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-white px-4 text-center">
-      <div className="w-60 sm:w-72 md:w-80 lg:w-96 mb-6">
+      <div className="w-80 sm:w-96 md:w-[550px] lg:w-[750px] ">
         <Image
           src="/images/rights-and-honors-logo.webp"
           alt="Rights and Honors Logo"
