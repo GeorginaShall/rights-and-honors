144b44cd1f36f2e624a11b9697064c657dad1946
GeorginaShall <georgina.shall@creoshift.com>
2025-07-22 18:01:36 +0300

    crop img - larger

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-white px-4 text-center">
-      <div className="w-60 sm:w-72 md:w-80 lg:w-96 mb-6">
+      <div className="w-80 sm:w-96 md:w-[450px] lg:w-[750px] ">
         <Image
-          src="/images/rights-and-honors-logo.webp"
+          src="/images/rights-and-honors-logo-cropped.webp"
           alt="Rights and Honors Logo"
           width={400}
           height={400}
