8d4379bde548cd7a5d604f279c1d26452297eb36
GeorginaShall <georgina.shall@creoshift.com>
2025-07-23 15:20:27 +0300

    title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,18 @@
-import "../assets/scss/global.css";
-import "../assets/scss/main.scss";
+import '../assets/scss/global.css';
+import '../assets/scss/main.scss';
 
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>NextJs</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <title>Rights and Honors</title>
       </Head>
       <main>
         <Component {...pageProps} />
